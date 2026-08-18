87ca0e8b58ca5a886b38f4839639d9e53fc59f93
gamboa1234 <marco.gamboa@uab.edu.bo>
2026-08-18 17:00:20 -0400

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,22 @@
+// ================= MODO OSCURO =================
+
+const cuerpo = document.querySelector("body");
+const botonModo = document.querySelector("#btn-tema");
+
+let esDeDia = false;
+
+function alternarModo() {
+
+    cuerpo.classList.toggle("claro");
+
+    esDeDia = !esDeDia;
+
+    if (esDeDia) {
+        botonModo.textContent = "🌙 Modo Noche";
+    } else {
+        botonModo.textContent = "☀️ Modo Día";
+    }
+
+}
+
+botonModo.addEventListener("click", alternarModo);
